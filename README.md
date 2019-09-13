0486d33de1bf3b2866f1021c650dbf133317f00c
tami97 <tami19970811@gmail.com>
2019-09-13 09:48:39 +0200

    Costumizing figure in analysis.R and adding describtion to README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# eltecon-ds-class
+analysis.R:
+
+A kód létrehoz egy ábrát, ami az eladások számát mutatja be a vevők típusai szerint.
+  hisztogram
+    X: costumer lifecycle status
+    Y: count
--- analysis.R
+++ analysis.R
@@ -3,10 +3,13 @@ library(data.table)
 
 sales <- fread("data/sales.csv")
 
-ggplot(sales, aes(x = customer_lifecycle_status)) +
-  geom_bar() +  
-  geom_label(stat = 'count', aes(label = ..count..)) +
+ggplot(sales, aes(x = customer_lifecycle_status, fill = customer_lifecycle_status)) +
+  geom_bar() + 
+  geom_label(stat = 'count', aes(label = ..count..)) + 
   labs(
-    title = "Sales count by costumer lifecycle status",
+    title = "Number of sales by costumer lifecycle status",
     x = "costumer lifecycle status"
-  )
+  ) + 
+  theme_minimal() + 
+  theme(legend.position = "none", plot.title = element_text(hjust = 0.5)) + 
+  scale_fill_brewer(palette="BuGn")
